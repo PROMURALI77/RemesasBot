8361b1c5bf92d4eb246c9b48e5a8dfcd93759e1c
Samuel <89231917+SamuelDev00@users.noreply.github.com>
2022-04-04 18:08:54 -0400

    Update _onLTC.js

--- commands/_onLTC.js
+++ commands/_onLTC.js
@@ -13,8 +13,6 @@ CMD*/
 
 var data = JSON.parse(content)
 
-let price = data.ticker.price
-let x = price*1
 let wallet = options.result.address;
 Bot.sendMessage("⚠️ _If you send less than " +x.toFixed(8)+ " LTC, your deposit will be igonred!_\n\n✅* Send the amount you want to invest to the following address:*")
 Bot.sendMessage("`" + wallet + "`")
