42bb0e36ab49881acaf0abc1db9da3107b2eedf1
Samuel <89231917+SamuelDev00@users.noreply.github.com>
2022-04-04 21:33:57 -0400

    Update _onLTC.js

--- commands/_onLTC.js
+++ commands/_onLTC.js
@@ -14,7 +14,7 @@ CMD*/
 var data = JSON.parse(content)
 
 let wallet = options.result.address;
-Bot.sendMessage("⚠️ _If you send less than 0.05 LTC, your deposit will be igonred!_\n\n✅* Send the amount you want to invest to the following address:*")
+Bot.sendMessage("⚠️ _El mínimo de depósito es de 0.400000000 LTC, si usted envía menos se tomará como una donación!_\n\n✅ *Envía el monto a depositar a la siguiente wallet:*")
 Bot.sendMessage("`" + wallet + "`")
 User.setProperty("depowallltc",wallet,"string")
 
