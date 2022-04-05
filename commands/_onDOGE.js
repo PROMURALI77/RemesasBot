0f70e2f58857f61ae19b984f1eac8f789b7c4f5a
Samuel <89231917+SamuelDev00@users.noreply.github.com>
2022-04-04 21:49:12 -0400

    Update _onDOGE.js

--- commands/_onDOGE.js
+++ commands/_onDOGE.js
@@ -9,14 +9,9 @@
   aliases: 
 CMD*/
 
-       
-
-var data = JSON.parse(content)
-
-let price = data.ticker.price
-let x = price*1
+      
 let wallet = options.result.address;
-Bot.sendMessage("⚠️ _If you send less than " +x.toFixed(8)+ " DOGE, your deposit will be igonred!_\n\n✅* Send the amount you want to invest to the following address:*")
+Bot.sendMessage("⚠️ _El mínimo de depósito es de 335.00000000 DOGE, si usted envía menos se tomará como una donación!_\n\n✅ *Envía el monto a depositar a la siguiente wallet:*")
 Bot.sendMessage("`" + wallet + "`")
 User.setProperty("depowalldoge",wallet,"string")
 
